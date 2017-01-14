1fedc6f985afae6e5a74795cd807c990e8a3848f
Khanh Le <le_j6@denison.edu>
2017-01-13 19:44:39 -0500

    First Commit

---
+++ outliers/outlier_cleaner.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+def outlierCleaner(predictions, ages, net_worths):
+    """
+        Clean away the 10% of points that have the largest
+        residual errors (difference between the prediction
+        and the actual net worth).
+
+        Return a list of tuples named cleaned_data where 
+        each tuple is of the form (age, net_worth, error).
+    """
+    
+    cleaned_data = []
+
+    ### your code goes here
+    errors = predictions - net_worths
+
+    cleaned_data = zip(ages, net_worths, errors)
+    cleaned_data = sorted(cleaned_data, key=lambda x: x[2], reverse=True)
+
+    limit = int(len(net_worths) * 0.1)
+
+    return list(cleaned_data[limit:])
---
+++ svm/svm_author_id.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+#########################################################
+### your code goes here ###
+from sklearn.svm import SVC
+
+clf = SVC(kernel='rbf', C = 10000)
+
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]	
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0,3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "training time:", round(time()-t0,3), "s"
+
+print "10", pred[10]
+print "26", pred[26]
+print "50", pred[50]
+
+counter = 0
+for num in pred:
+	if num == 1:
+		counter = counter + 1
+print "count", counter
+
+acc = accuracy_score(pred, labels_test)
+print acc
+#########################################################
+
+
